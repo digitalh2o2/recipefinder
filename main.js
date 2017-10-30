712056a1c009aadad1a32db94a234318a898fd40
Saul <digitalh2o@gmail.com>
2017-10-30 17:23:49 -0500

    Pulling images and titles. Need to add classes for DOM manipulation

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Variables //
 
 const search = document.querySelector('input');
+const allColumns = document.querySelectorAll('.column');
 let button = document.querySelector('.button');
 
 const key = config.key;
@@ -21,7 +22,25 @@ function getPromise(){
   .then(function(response){
     response.json()
     .then(function(response){
-      console.log(response)
+      let recipes = response.recipes
+      console.log(recipes);
+      if(recipes.length > 0){
+        for(let i = 1; i < allColumns.length; i++){
+          allColumns[i].innerHTML = `
+            <h3>${recipes[i-1].title}</h3>
+            <figure class="image is-square">
+              <img src="${recipes[i-1].image_url}" alt="Recipe Results">
+            </figure>
+          `
+          allColumns[0].style.visibility = "hidden";
+        }
+      } else {
+        allColumns[0].style.visibility = "visible";
+        allColumns[0].innerHTML = `
+          <h3>No recipes containing ${userSearch} were found!</h3>
+        `
+      }
+
     })
     .catch(function(err){
       console.log(err);

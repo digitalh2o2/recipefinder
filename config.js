fd1cdd415cc80e193df8077d5d5239360f5bdcef
Saul <digitalh2o@gmail.com>
2017-10-31 14:54:00 -0500

    Remove gitignore file for gh-pages

---
+++ config.js
@@ -0,0 +1,8 @@
+const mashHeaders = {
+  "X-Mashape-Key": "kmMUqL1aUMmshlT96sRXbRefx9RDp1nfjHmjsnEFEgJBSlbhzY",
+  "Accept": "application/json"
+}
+
+const config = {
+  "key": "3db119e565c8630e6aecb0586bfed54c"
+}

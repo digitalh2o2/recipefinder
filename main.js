345d0af0410852a2d9e2b77a21da30fb6634f033
Saul <digitalh2o@gmail.com>
2017-10-30 19:30:42 -0500

    Links and additional Event listeners

--- main.js
+++ main.js
@@ -8,6 +8,11 @@ const key = config.key;
 const mashH = mashHeaders;
 
 button.addEventListener('click', getPromise);
+search.addEventListener('keyup', function(e){
+  if(e.keyCode === 13){
+    getPromise();
+  }
+})
 
 // Promise and Headers //
 function getPromise(){
@@ -22,25 +27,24 @@ function getPromise(){
   .then(function(response){
     response.json()
     .then(function(response){
-      let recipes = response.recipes
-      console.log(recipes);
+      let recipes = response.recipes;
       if(recipes.length > 0){
         for(let i = 1; i < allColumns.length; i++){
           allColumns[i].innerHTML = `
-            <h3>${recipes[i-1].title}</h3>
+            <h2><a href="${recipes[i-1].source_url}">${recipes[i-1].title}</a></h2>
+            <p>By: ${recipes[i-1].publisher}</p>
             <figure class="image is-square">
               <img src="${recipes[i-1].image_url}" alt="Recipe Results">
             </figure>
           `
-          allColumns[0].style.visibility = "hidden";
+          allColumns[0].style.display = "none"
         }
       } else {
-        allColumns[0].style.visibility = "visible";
+        allColumns[0].style.display = "";
         allColumns[0].innerHTML = `
           <h3>No recipes containing ${userSearch} were found!</h3>
         `
       }
-
     })
     .catch(function(err){
       console.log(err);
